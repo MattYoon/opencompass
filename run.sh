ea6b382537d3329928aaedd20f78e6f215bf3c72
MattYoon <mattyoon99@gmail.com>
2024-12-03 10:52:43 +0900

    modified run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python run.py --datasets mgsm_gen_679720 --hf-type base --hf-path Qwen/Qwen2.5-1.5B --hf-num-gpus 1 --debug -a vllm --batch-size 256
+python run.py --datasets pmmeval_gen_filter --hf-type chat --hf-path Qwen/Qwen2.5-1.5B-Instruct --hf-num-gpus 1 --debug -a vllm --batch-size 256
